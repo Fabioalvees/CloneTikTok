d5e3dd26a0cafad8b846770abb53c18be643754e
Fabio Alves Rosa <faabioalvesrosa@gmail.com>
2023-04-01 19:43:25 -0300

    third commit

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import VideoFooter from './components/footer/VideoFooter';
 import "./video.css"
 
 function Video() {
@@ -25,10 +26,13 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
       >
-
       </video>
+      {/*Side bar*/}
+
+      <VideoFooter/>
+      {/*footer*/}
     </div>
   );
 }
